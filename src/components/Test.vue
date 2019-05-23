9a17ce77a6d270037ac21d245c3bfb29d8dfd6fa
Luis Fernando <naraku@debian.debera>
2019-05-22 23:52:24 -0300

    Initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="test">
+        <input type="text" v-model="title"/><br/>
+        <h1>{{title}}</h1>
+        <h2>{{msg}}</h2>
+        <p v-if="showName">{{user.firstName}}</p>
+        <p v-if="showName" v-text='user.lastName'></p>
+        <p v-else="showName">Nobody</p>
+        <ul>
+            <li v-for="item in items">{{item.title}}</li>
+        </ul>
+        <button v-on:click="greet('Hello World!')">Say Greetings</button><br/>
+        <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit"/>
+        <hr/>
+        <label>First Name: </label><input type="text" v-model="user.firstName"/>
+        <br/>
+        <label>Last Name: </label><input type="text" v-model="user.lastName"/>
+        <br/>
+        <h3>{{fullName}}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'test',
+        props: {
+            msg: {
+                type: String,
+                default: 'Foobar',
+            }
+        },
+        data(){
+            return {
+                title: 'Hello world',
+                user: {
+                    firstName: 'Dino',
+                    lastName: 'Sauro'
+                },
+                showName: true,
+                items:[
+                    {title: 'Item One'},
+                    {title: 'Item Two'},
+                    {title: 'Item Three'}
+                ]
+            }
+        },
+        methods:{
+            greet: function(greet){
+                alert(greet)
+            },
+            pressKey: function(e){
+                console.log(e.target.value)
+            },
+            enterHit: function(){
+                console.log('You hit enter')
+            }
+        },
+        computed: {
+            fullName:  function(){
+                return `${this.user.firstName} ${this.user.lastName}`
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
